321632d1ff19fc1fde4e5634704e42b6c94c4297
TranChiHuynh200065 <huynhtcgcc200065@fpt.edu.vn>
2022-07-05 08:07:11 +0700

    Huynh

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -22,6 +22,7 @@ class ProductController extends AbstractController
     {
         $product = $repo->findAll();
         
+     
         return $this->render('product/index.html.twig', [
             'products' => $product
         ]);
